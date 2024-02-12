aaf3cb8fea5683d38d3433858e6789892e720d14
Asaduzzaman <masaduzzaman193146@bscse.uiu.ac.bd>
2024-02-13 02:16:55 +0600

    [DONE] partial error handling

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
+import globalErrorHandler from './app/middleware/globalErrorHandler';
 
 const app = express();
 
@@ -7,8 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res, next) => {
+//   // throw new Error('Normal Error');
+//   throw new ApiError(400, 'custom error');
+//   // next('custom error');
+// });
+
+app.use(globalErrorHandler);
 
 export default app;
